e213511928637574889fb61ea1c389bafe79c175
Trong Binh <trongbinhnguyen15@gmail.com>
2022-01-10 11:09:42 +0700

    init commit

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Pokedex from 'pokedex-promise-v2';
+
+const P = new Pokedex();
+
+
+//  P.getPokemonByName(['eevee', 'ditto']) // with Promise
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log('There was an ERROR: ', error);
+//   });
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
